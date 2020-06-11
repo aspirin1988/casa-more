8f8900855f01f13e6e23ded16697f9b13e820f6d
serg <aspirins24@gmail.com>
2020-06-11 14:11:57 +0600

    Init

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
                 phone.addEventListener('keyup', function (e) {
                     let phone_val = phone.value;
                     let array = ['+', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
+                    console.log(e.key);
                     console.log(array.indexOf(e.key));
 
                     if (array[e.key] !== undefined) {
